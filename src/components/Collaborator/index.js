856aae82fe1477045976ebc8b530f2d8152c98dd
Bruno Soares <brunosoaresbsdev@gmail.com>
2024-04-13 18:40:51 -0300

    Collaborator component created

---
+++ src/components/Collaborator/index.js
@@ -0,0 +1,17 @@
+import './Collaborator.css';
+
+const Collaborator = () => {
+    return (
+        <div className='collaborator'>
+            <div className='header'>
+                <img src='https://github.com/brunosoaresbsdev.png' alt='Collaborator' />
+            </div>
+            <div className='footer'>
+                <h4>Bruno Soares</h4>
+                <h5>Back-end dev</h5>
+            </div>
+        </div>
+    );
+};
+
+export default Collaborator;
